3e55ba15fb1e5387a023caaa9690e8229d47cfe8
RAPHAEL HENRIQUE SANTOS DE OLIVEIRA <26171669@edu119.local>
2026-09-17 15:34:06 -0300

    Salvando

---
+++ db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE db_backend;
+
+CREATE TABLE IF NOT EXISTS alunos(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR (100) NOT NULL,
+	curso VARCHAR (100) NOT NULL
+);
+
+INSERT INTO alunos (nome, curso)
+VALUES
+	("Bernardo", "Desenvolvimento de sistema"),
+	("Camily", "Banco de dados");
+
+SELECT id, nome, curso FROM alunos;
--- servidor.js
+++ servidor.js
@@ -1,24 +1,89 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-let alunos = [
-    {id: 1, nome: 'João', curso: 'Desenvolvimento de Sistemas'},
-    {id: 2, nome: 'Maria', curso: 'Rede de Computadores'},
-    {id: 3, nome: 'Pedro', curso: 'Banco de Dados'},
-    {id: 4, nome: 'Ana', curso: 'Administração'},
-    {id: 5, nome: 'Carlos', curso: 'Desenvolvimento de Sistemas'},
+let ALUNOS = [
+    {id: 1, nome: "Bernardo", curso: "Desenvolvimento de sistemas"},
+    {id: 2, nome: "Camily", curso: "Redes de computadores"},
+    {id: 3, nome: "Kaue", curso: "Banco de dados"},
+    {id: 4, nome: "Maria", curso: "Administração"},
+    {id: 5, nome: "Marjory", curso: "Desenvolvimento de sistemas"},
 ];
 
-app.get('/', (req, res) => {
+app.get("/",(req,res) =>{
     res.json({
-        menssagem: 'API Alunos funcionando'
+        mensagem: "API Alunos funcionando"
     })
 });
 
-const PORTA = 3000;
-app.listen(PORTA, () => {
-    console.log(`Servidor rodando na porta ${PORTA}`);
-    console.log(`Acesse: http://localhost:${PORTA}`);
+app.get("/alunos",(req, res) =>{
+    res.json(ALUNOS);
+});
+
+app.post("/alunos/cadastrar", (req, res)=>{
+    // console.log(req.body);
+    const {nome, curso} = req.body
+    // console.log("Nome:" + nome);
+    // console.log(Curso: ${curso});
+
+    if(!nome || !curso){
+        return res.status(400).json({msg: "Nome e curso são obrigatorio"})
+    }
+
+    const id = ALUNOS.length > 0 ? ALUNOS[ALUNOS.length - 1].id + 1 : 1;                                     
+                                                         
+    const novoAluno = {            
+        id : id,
+        nome : nome,
+        curso : curso
+    };
+
+    ALUNOS.push(novoAluno);
+
+    res.status(201).json({msg: "Aluno cadastrado com sucesso."})
+
+});
+
+app.get("/alunos/:valor", (req,res) =>{
+    console.log(req)
+    const valor = Number(req.params.valor);
+
+    const aluno = ALUNOS.find(aluno => aluno.id === valor);
+    
+    if(!aluno){
+        return res.status(404).json({msg: "Aluno não encontrado"});
+    }
+    res.status(200).json(aluno);
+});
+
+app.put("/alunos/:valor",(req, res)=>{
+    const valor = Number(req.params.valor);
+    const {nome, curso} = req.body;
+
+    if(!nome || !curso){
+        return res.status(400).json({msg: "Nome e curso são obrigatorio"})
+    }
+
+    const indice = ALUNOS.findIndex(aluno => aluno.id === valor);
+    // console.log(indice)
+    if(indice === -1){
+        return res.status(404).json({msg: "Aluno não encontrado"})
+    }
+
+    ALUNOS[indice] ={
+        id: valor,
+        nome : nome,
+        curso: curso
+    };
+    
+    res.status(200).json({msg: "Aluno atualizado com sucesso"});
+
 })
 
+
+const PORTA = 3000;
+app.listen(PORTA, ()=>{
+    console.log("Servidor iniciado com sucesso");
+    console.log(`http://localhost:${PORTA}`);
+})
